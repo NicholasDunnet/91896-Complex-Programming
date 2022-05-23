3ce8b059bf5f8da2e8d7f0bf6b31402bf1e04260
Nicholas <nicholasdunnet@gmail.co.nz>
2022-05-23 13:05:43 +1200

    Made buttons work with images

--- mainmenu.py
+++ mainmenu.py
@@ -1,27 +1,41 @@
 # importing modules
-import tkinter as tk
+import tkinter as tk       
+import pathlib
+import os
+
+# creating helper functions
+def get_image_path(filename):
+    current_dir = pathlib.Path(__file__).parent.resolve() # current directory
+    current_dir = os.path.join(current_dir, "assets/") # join with assets folder
+    img_path = os.path.join(current_dir, filename) # join with your image's file name
+    return img_path
 
 # setting up window
 root = tk.Tk()
-root.title('TIMUS')
+root.title('TIMUS')    
 root.geometry('600x400')
 
 content = tk.Frame(root, padx=12, pady=12, bg='#ff0000')
 content.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
 
-titlelabel = tk.Label(content, text='Timus', bg='#ff0000')
+titleimage= tk.PhotoImage(file=get_image_path("title.png"))
+titlelabel = tk.Label(content, image=titleimage, bg='#ff0000')
 titlelabel.grid(row=0, column=0, rowspan=2, columnspan=5, sticky=tk.N+tk.S+tk.E+tk.W)
 
-startbutton = tk.Button(content, borderwidth = 10, background = 'white', relief = 'groove', text='Start', height=1, command=lambda: root.destroy())
+startimage = tk.PhotoImage(file=get_image_path("Start.png"))
+startbutton = tk.Button(content, borderwidth = 10, background = 'white', relief = 'groove', image=startimage, height=1, command=lambda: root.destroy())
 startbutton.grid(row=2, column=2, rowspan=1, columnspan= 1, sticky=tk.N+tk.S+tk.E+tk.W, padx=5, pady=5)
 
-instructionsbutton = tk.Button(content, borderwidth = 10, background = 'white', relief = 'groove', text='Instructions', height=1, command=lambda: root.destroy())
+instructionsimage = tk.PhotoImage(file=get_image_path("Instructions.png"))
+instructionsbutton = tk.Button(content, borderwidth = 10, background = 'white', relief = 'groove', image=instructionsimage, height=1, command=lambda: root.destroy())
 instructionsbutton.grid(row=3, column=2, rowspan=1, columnspan= 1, sticky=tk.N+tk.S+tk.E+tk.W, padx=5, pady=5)
 
-settingsbutton = tk.Button(content, borderwidth = 10, background = 'white', relief = 'groove', text='Settings', height=1, command=lambda: root.destroy())
+settingsimage = tk.PhotoImage(file=get_image_path("settings.png"))
+settingsbutton = tk.Button(content, borderwidth = 10, background = 'white', relief = 'groove', image=settingsimage, height=1, command=lambda: root.destroy())
 settingsbutton.grid(row=4, column=2, rowspan=1, columnspan= 1, sticky=tk.N+tk.S+tk.E+tk.W, padx=5, pady=5)
 
-quitbutton = tk.Button(content, borderwidth = 10, background = 'white', relief = 'groove', text='Quit', height=1, command=lambda: root.destroy())
+quitimage = tk.PhotoImage(file=get_image_path("Quit.png"))
+quitbutton = tk.Button(content, borderwidth = 10, background = 'white', relief = 'groove', image=quitimage, height=1, command=lambda: root.destroy())
 quitbutton.grid(row=5, column=2, rowspan=1, columnspan= 1, sticky=tk.N+tk.S+tk.E+tk.W, padx=5, pady=5)
 
 # configuring all rows and columns for resizing
@@ -33,5 +47,6 @@ for i in range (0, 5):
 for i in range (0, 6):
     content.rowconfigure(i, weight=1)
 
-# loop the window
-root.mainloop()
+# looping
+while True:
+    root.update()
