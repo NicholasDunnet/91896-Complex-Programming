8dc07fd4e855c8a0ba4c43f4ed46d55bbbf282bc
Nicholas <nicholasdunnet@gmail.co.nz>
2022-05-24 15:02:30 +1200

    Started creating the GameWindow

--- mainmenu.py
+++ mainmenu.py
@@ -2,6 +2,8 @@
 import tkinter as tk       
 import pathlib
 import os
+import random
+from turtle import position
 
 # creating global functions
 def getimagepath(filename):
@@ -10,10 +12,38 @@ def getimagepath(filename):
         img_path = os.path.join(current_dir, filename) # join with your image's file name
         return img_path
 
-class Game:
+class GameWindow:
     def __init__(self):
-        print("GameGUI")
+        # Create window
+        self.root = tk.Tk()
+        self.root.title('TIMUS')
 
+        # Finds out users screen size
+        screenwidth = self.root.winfo_screenwidth()
+        screenheight = self.root.winfo_screenheight()
+
+        # Randomizes the size of the window which is to be created
+        windowwidth = random.randint(100, 500)
+        windowheight = random.randint(100, 350)
+
+        # Figures out where to place the window on the screen
+        xpos = random.randint(0, screenwidth - windowwidth)
+        ypos = random.randint(0, screenheight - windowheight)
+
+        # Places the window on the screen and disables resizing of windows
+        self.root.geometry(f"{windowwidth}x{windowheight}+{xpos}+{ypos}")
+        self.root.resizable(False, False)
+
+        # Set window close action to close all windows
+        self.root.protocol("WM_DELETE_WINDOW",lambda: MainMenu.del_windows(MainMenu))
+
+        # Allows the window to function with other windows and not be blocked
+        self.root.after(1000, self.loop)
+
+    # Creates a loop which will run every second for each window
+    def loop(self):
+        self.root.after(1000, self.loop)
+        
 class Instructions:
     def __init__(self):
         print("SettingsGUI")
@@ -65,14 +95,15 @@ class MainMenu:
         for i in range (0, 6):
             content.rowconfigure(i, weight=1)
 
-        # looping
-        while True:
-            self.root.update()
+        # Create array of windows.
+        MainMenu.windows = []
 
-    # creating gui functions
+        # looping
+        self.root.mainloop()
+
     def start(self):
-        self.game = Game()
-        self.root.destroy()
+        self.add_window()
+        # self.root.destroy()
 
     def instructions(self):
         self.instructions = Instructions()
@@ -81,6 +112,21 @@ class MainMenu:
     def settings(self):
         self.settings = Settings()
         self.root.destroy()
+
+    def add_window(self):
+        # Create a new window by creating a new Window instance.
+        w = GameWindow()
+        # Save this Window to the list of windows.
+        MainMenu.windows.append(w)
+
+    def del_windows(self):
+        # Close all windows.
+        for window in MainMenu.windows:
+            try:
+                window.root.destroy()
+            except:
+                continue
+        application = MainMenu()
 
 # starts the main menu
 application = MainMenu()
